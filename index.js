ceb26a1dedf59426a9f5d714e0a64e07ae7d1147
sanatiel90 <sanatielbarros@gmail.com>
2017-10-25 22:42:53 -0300

    testando rotas

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cassandra = require('cassandra-driver');
 var bodyParser = require('body-parser');
+ 
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended:true }));
 
 var client = new cassandra.Client({contactPoints:['127.0.0.1'], keyspace:'trab_spd'});
 
@@ -13,19 +16,30 @@ client.connect(function(err, result){
 
 app.get("/contato", function(req,res){
     
-    var q = "SELECT * FROM trab_spd.contatos WHERE num = 4";
-    var params = 4;
-    
-    client.execute(q,[],function(err, result){
+    var q = "SELECT * FROM trab_spd.contatos WHERE nome = ?";
+   
+    client.execute(q,[req.query.nome], function(err, result){
         if(err){
-            res.status(404).send({"msg":err});
+            res.status(401).send({"msdg":"erro"});
         }
 
-        var user = result.rows[0];
-
-        res.json({"nome":user.nome});
+      
+        try {
+            var user = result.rows[0]; 
+            res.json({"nome":user.nome,"tel":user.tel,"num":user.num}); 
+        } catch (error) {
+            res.status(401).json({"msg":"erro"});
+        }       
+        
+       
 
     });
+
+   
+
+
+
+
 
 });
 
@@ -34,9 +48,11 @@ app.post("/contato", function(req, res){
 
     var q = "INSERT INTO trab_spd.contatos(num,nome,tel) VALUES(?,?,?)";
 
-    client.execute(q,[],function(err, result){
+    var params = [req.body.num,req.body.nome,req.body.tel]; 
+
+    client.execute(q,params,{prepare:true}, function(err, result){
         if(err){
-            res.status(404).send({"msg":err});
+            res.status(404).send({"msg":"erro"});
         }
 
         res.status(201).json({"msg":"criado com sucesso"});
